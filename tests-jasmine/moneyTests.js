cf17612c86d32c96d9f28a86e8ff8f1981cc8bfb
tazy-dev <tazykaharaba2@gmail.com>
2025-01-19 21:00:25 +0200

    Testing money.js File using Jasmine and removing old Files

---
+++ tests-jasmine/moneyTests.js
@@ -0,0 +1,14 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("Test Suite : formatCurrency", ()=>{
+    it("converts 1000 cents to $10.00",()=>{
+        expect(formatCurrency(1000)).toEqual("10.00");
+    });
+    it("converts  0 cents to $0.00",()=>{
+        expect(formatCurrency(0)).toEqual("0.00");
+    });
+    it("converts 2000.5 cents to $20.01",()=>{
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+})
+
