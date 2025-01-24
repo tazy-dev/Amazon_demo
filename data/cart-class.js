ed0b2bbb2f89e9acbac54e1844311596550e7ece
tazy-dev <tazykaharaba2@gmail.com>
2025-01-25 00:27:00 +0200

    Using Classes

---
+++ data/cart-class.js
@@ -0,0 +1,95 @@
+class Cart {
+  constructor(storageKey) {
+    this.localStorageKey = storageKey;
+    this.loadFromStorage();
+    this.cartQuantity = this.getQuantity();
+  }
+  cartItems ;
+  localStorageKey ;
+  cartQuantity;
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+  getQuantity() {
+    var quantity = 0;
+    this.cartItems.forEach((cartItem) => {
+      quantity += cartItem.quantity;
+    });
+    
+    return quantity;
+  }
+
+  addToCart(productId, quantity) {
+    let itemFound = false;
+    this.cartItems.forEach((item) => {
+      if (item.productId === productId) {
+        item.quantity += quantity;
+        itemFound = true;
+      }
+    });
+    if (!itemFound) {
+      this.cartItems.push({
+        productId,
+        quantity,
+        deliveryOptionId: "1",
+      });
+    }
+    this.cartQuantity += quantity;
+    this.saveToStorage();
+  }
+
+  updateCartQunatity(productId, quantity) {
+    this.cartItems.forEach((item) => {
+      if (item.productId === productId) {
+        this.cartQuantity -= item.quantity;
+
+        item.quantity = quantity;
+      }
+    });
+
+    this.cartQuantity += quantity;
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    this.cartItems = this.cartItems.filter((cartItem) => {
+      if (cartItem.productId !== productId) {
+        return true;
+      } else {
+        this.cartQuantity -= cartItem.quantity;
+        return false;
+      }
+    });
+
+    this.saveToStorage();
+  }
+  updateCartDeliveryOption(productId, deliveryOptionId) {
+    this.cartItems.forEach((item) => {
+      if (item.productId === productId) {
+        item.deliveryOptionId = deliveryOptionId;
+      }
+    });
+    this.this.saveToStorage();
+  }
+}
+
+
+const userCart = new Cart(
+    'cart-oop'
+)
+const businessCart = new  Cart(
+    'cart-business'
+)
+userCart.addToCart("15b6fc6f-327a-4ec4-896f-486349e85a3d",10);
+businessCart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6",10);
+console.log(userCart);
+console.log(businessCart);
+
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 
 import { renderOrderSummary } from "./checkout/order-summery.js";
 import { renderPaymentSummary } from "./checkout/payment-summery.js"
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 
 renderOrderSummary();
 
