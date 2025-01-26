bf51438368221d693a65b05a65876db2fd3ea93f
tazy-dev <tazykaharaba2@gmail.com>
2025-01-27 01:00:23 +0200

    unused Import

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 
 import { renderOrderSummary } from "./checkout/order-summery.js";
 import { renderPaymentSummary } from "./checkout/payment-summery.js"
-import { loadProjects,loadProdects} from "../data/products.js";
+import { loadProdects} from "../data/products.js";
 
 
         loadProdects().then(()=>{
