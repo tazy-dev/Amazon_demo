7a4ee65b216ee9d8d8bb72297729f0ce8bfef9e8
tazy-dev <tazykaharaba2@gmail.com>
2025-01-27 00:27:43 +0200

    Using Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,9 +6,23 @@ import { loadProjects} from "../data/products.js";
 // import '../data/cart-class.js'
 
 //import '../data/backend-practice.js'
-loadProjects(()=>{
-    renderOrderSummary();
 
+// Practice Promises
+// It Creates a Seperate line of excution
+ new Promise((resolve)=>{
+    console.log("Start");
+    loadProjects(()=>{
+        console.log("Finished Loading");
+        resolve("Rendering Layout");
+    })
+ }).then((value)=>{
+    console.log(value);
+    renderOrderSummary(); 
     renderPaymentSummary();
-})
+ })
+// loadProjects(()=>{
+//     renderOrderSummary();
+
+//     renderPaymentSummary();
+// })
 
